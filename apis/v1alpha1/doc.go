377b7f5d733a89f26df12136eb32c69ef9383987
Thomas Perelle <thomas@gravitek.io>
2025-10-08 06:19:37 +0200

    refactor: rename core API group from ovhcloud to ovh
    Renamed ProviderConfig, ProviderConfigUsage and StoreConfig API group
    from ovhcloud.crossplane.io to ovh.crossplane.io for consistency with
    the provider name.
    
    Changes:
    - apis/v1alpha1/doc.go: ovhcloud.crossplane.io → ovh.crossplane.io
    - apis/v1beta1/doc.go: ovhcloud.crossplane.io → ovh.crossplane.io
    - Regenerated core CRDs with new API group
    
    This is a breaking change: existing ProviderConfigs must be recreated
    with the new apiVersion ovh.crossplane.io/v1beta1.

--- apis/v1alpha1/doc.go
+++ apis/v1alpha1/doc.go
@@ -2,8 +2,8 @@
 Copyright 2021 Upbound Inc.
 */
 
-// Package v1alpha1 contains the core resources of the ovhcloud jet provider.
+// Package v1alpha1 contains the core resources of the ovh jet provider.
 // +kubebuilder:object:generate=true
-// +groupName=ovhcloud.crossplane.io
+// +groupName=ovh.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
--- apis/v1beta1/doc.go
+++ apis/v1beta1/doc.go
@@ -2,8 +2,8 @@
 Copyright 2022 Upbound Inc.
 */
 
-// Package v1beta1 contains the core resources of the ovhcloud upjet provider.
+// Package v1beta1 contains the core resources of the ovh upjet provider.
 // +kubebuilder:object:generate=true
-// +groupName=ovhcloud.crossplane.io
+// +groupName=ovh.crossplane.io
 // +versionName=v1beta1
 package v1beta1
